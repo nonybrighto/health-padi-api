8f2b814b4d2d68be6d817dde1064de2085c056a5
nonybright <nonybrighto@gmail.com>
2020-08-27 21:47:52 +0100

    Change image url

--- src/app/sequelize/seeders/20200225102740-demo-news.js
+++ src/app/sequelize/seeders/20200225102740-demo-news.js
@@ -4,10 +4,11 @@ module.exports = {
       'News',
       [
         {
-          title: 'maintian a good health',
+          title: 'this is the first',
           host: 'health.ng',
           sourceUrl: 'http://www.google.com',
-          imageUrl: 'http://imageUrl.com',
+          imageUrl:
+            'https://igbotrade.s3.us-east-2.amazonaws.com/post_images/1580305153466.jpg',
           summary: 'this is the summary of the news',
           publishedAt: '2019-01-01 02:00:00',
           crawledAt: '2019-01-01 02:00:00',
@@ -15,10 +16,11 @@ module.exports = {
           updatedAt: '2019-01-01 02:00:00'
         },
         {
-          title: 'maintian a good health',
+          title: 'this is the second',
           host: 'health.ng',
           sourceUrl: 'http://www.google.com',
-          imageUrl: 'http://imageUrl.com',
+          imageUrl:
+            'https://igbotrade.s3.us-east-2.amazonaws.com/post_images/1580305153466.jpg',
           summary: 'this is the summary of the news',
           publishedAt: '2019-01-01 02:00:00',
           crawledAt: '2019-01-01 02:00:00',
@@ -26,10 +28,23 @@ module.exports = {
           updatedAt: '2019-01-01 02:00:00'
         },
         {
-          title: 'maintian a good health',
+          title: 'another awesome nonsense content for you to see',
           host: 'last.ng',
           sourceUrl: 'http://www.google.com',
-          imageUrl: 'http://imageUrl.com',
+          imageUrl:
+            'https://igbotrade.s3.us-east-2.amazonaws.com/post_images/1580305153466.jpg',
+          summary: 'this is the summary of the news',
+          publishedAt: '2019-01-01 02:00:00',
+          crawledAt: '2019-01-01 02:00:00',
+          createdAt: '2019-01-04 02:00:00',
+          updatedAt: '2019-01-01 02:00:00'
+        },
+        {
+          title: 'maintian a good health and always show',
+          host: 'last.ng',
+          sourceUrl: 'http://www.google.com',
+          imageUrl:
+            'https://igbotrade.s3.us-east-2.amazonaws.com/post_images/1580305153466.jpg',
           summary: 'this is the summary of the news',
           publishedAt: '2019-01-01 02:00:00',
           crawledAt: '2019-01-01 02:00:00',
