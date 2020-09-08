6ee68749ab29275901c56976df316873a1288b7d
nonybright <nonybrighto@gmail.com>
2020-09-08 10:18:25 +0100

    Log internal error

--- src/app/helpers/internal-error.js
+++ src/app/helpers/internal-error.js
@@ -1,9 +1,22 @@
 import createError from 'http-errors';
 import httpStatus from 'http-status';
+import fs from 'fs';
+import path from 'path';
 
 function internalError(message, error) {
-  // eslint-disable-next-line no-console
   console.log(error);
+  const dateString = new Date().toString();
+  fs.appendFile(
+    path.join(__dirname, '../../../morgan.log'),
+    `~~~~~\n 
+    Internal error occured while ${message} - ${dateString}\n 
+    ${error} - ${error.stack}\n~~~~~~~\n`,
+    err => {
+      if (err) {
+        console.log('error occured while adding');
+      }
+    }
+  );
   return createError(
     httpStatus.INTERNAL_SERVER_ERROR,
     `Internal error occured while ${message}`,
